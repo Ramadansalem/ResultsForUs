f9b4107f1da7bdb06d54bff51a638c36d54ff39d
Ramadansalem <r.aliuosef@gmail.com>
2024-12-27 09:56:38 +0200

    Add files via upload

---
+++ results.js
@@ -0,0 +1,385 @@
+const data = {
+  students: [
+    {
+      id: "1",
+      name: "علي مصطفى علي",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.5,
+          total: "13.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.5,
+          total: "13.5 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3 / 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.83,
+          total: "13.83 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "2",
+      name: "علي جمعة محمد",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3 / 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4,
+          total: "13 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4,
+          total: "13 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 6.16,
+          total: "15.16 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "3",
+      name: "محمد يونس بلقاسم",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 6,
+          total: "15 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.83,
+          total: "13.83 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 6.5,
+          total: "15.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "4",
+      name: "بلقاسم يونس بلقاسم",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.5,
+          total: "13.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5,
+          total: "14 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.16,
+          total: "14.16 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "5",
+      name: "علي سعيد سعد",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 3.83,
+          total: "12.83 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.83,
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.83,
+          total: "14.83 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "6",
+      name: "معتز صالح محمد",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 3.5,
+          total: "12.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.16,
+          total: "13.16 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.16,
+          total: "13.16 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "7",
+      name: "سعدة عبدالله عبدالنبي",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.66,
+          total: "14.66 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.56,
+          total: "14.56 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 4.83,
+          total: "13.83 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "8",
+      name: "ام العز خليل بوشويشة",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 3.5,
+          total: "12.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.3,
+          total: "14.3 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: "غياب عن الامتحان",
+          total: "9 بدون درجة التطبيقات الشهرية و بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+    {
+      id: "9",
+      name: "رتاج امحمد عبد الخالق",
+      subjects: {
+        1: {
+          name: "مباديء تصميم مواقع الإنترنت",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 3.5,
+          total: "12.5 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        2: {
+          name: "تحليل وتصميم النظم",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 5.5,
+          total: "14.5 بدون درجة الحضور والغياب",
+
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+        3: {
+          name: "التدريبات العملية",
+          behavior: 3,
+          homework: 3,
+          notebook: 3,
+          attendance: "لم ترصد بعد",
+          monthlyApplications: 3.8,
+          "total": "12.8 بدون درجة الحضور والغياب",
+          finalExam: "لم ترصد بعد",
+          practicalExam: "لم ترصد بعد",
+        },
+      },
+    },
+  ],
+};
+
+function showResults() {
+  const studentId = document.getElementById("studentId").value;
+  const subjectId = document.getElementById("subjectId").value;
+  const student = data.students.find((s) => s.id === studentId);
+  const subject = student ? student.subjects[subjectId] : null;
+
+  if (student && subject) {
+    document.getElementById("result").innerHTML = `
+            <h2>اسم الطالب: ${student.name}</h2>
+            <h3>المادة: ${subject.name}</h3>
+            <p>درجة السلوك: ${subject.behavior}</p>
+            <p>درجة الواجبات: ${subject.homework}</p>
+            <p>درجة الكراسة: ${subject.notebook}</p>
+            <p>درجة الحضور والغياب: ${subject.attendance}</p>
+            <p>درجة التطبيقات الشهرية: ${subject.monthlyApplications}</p>
+            <p>الدرجة الكلية لاعمال الفصل الاول: ${subject.total}</p>
+            <p>درجة امتحان نهاية الفصل: ${subject.finalExam}</p>
+            <p>درجة امتحان العملي: ${subject.practicalExam}</p>
+        `;
+  } else {
+    document.getElementById("result").innerHTML = "<p>النتيجة غير متوفرة</p>";
+  }
+}
